cc6c3ee2e7fbd6ffc50e9fafb0384ab859d2f90a
HaaaamBurger <gemasclashes@gmail.com>
2025-10-16 23:03:38 +0300

    removed UserAlreadyExistsException

--- src/main/java/com/vendo/domain/user/common/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.vendo.domain.user.common.exception;
-
-@Deprecated
-public class UserAlreadyExistsException extends RuntimeException {
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
