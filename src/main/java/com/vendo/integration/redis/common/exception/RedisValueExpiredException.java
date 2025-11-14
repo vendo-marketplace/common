34558c6df79099a4d1c12ba54d7c353ca8e0bb04
HaaaamBurger <gemasclashes@gmail.com>
2025-11-14 20:11:02 +0200

    v1.0.9: ProviderType, OtpExpiredException

--- src/main/java/com/vendo/integration/redis/common/exception/RedisValueExpiredException.java
+++
@@ -1,7 +0,0 @@
-package com.vendo.integration.redis.common.exception;
-
-public class RedisValueExpiredException extends RuntimeException {
-    public RedisValueExpiredException(String message) {
-        super(message);
-    }
-}
