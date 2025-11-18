c4d6a5cfdc4e6b8c91780daa441ed1e5c363fbf4
HaaaamBurger <gemasclashes@gmail.com>
2025-11-19 00:44:53 +0200

    #23: removed type from ExceptionResponse, added errors

--- src/main/java/com/vendo/common/exception/ExceptionResponse.java
+++ src/main/java/com/vendo/common/exception/ExceptionResponse.java
@@ -1,11 +1,22 @@
 package com.vendo.common.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
+import lombok.NonNull;
+
+import java.util.Map;
 
 @Builder
 public record ExceptionResponse(
+        @NonNull
         String message,
-        String type,
+
+        @JsonInclude(JsonInclude.Include.NON_NULL)
+        Map<String, String> errors,
+
         int code,
         String path) {
 }
+
+
+
