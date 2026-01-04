a6dc7f2278118a03ed50914ade1f5c8c64f21814
HaaaamBurger <gemasclashes@gmail.com>
2026-01-04 14:07:30 +0200

    v1.3.14: user activity interface

---
+++ src/main/java/com/vendo/domain/user/service/UserActivityPolicy.java
@@ -0,0 +1,23 @@
+package com.vendo.domain.user.service;
+
+import com.vendo.domain.user.common.type.UserStatus;
+import com.vendo.security.common.exception.UserBlockedException;
+import com.vendo.security.common.exception.UserEmailNotVerifiedException;
+import com.vendo.security.common.exception.UserIsUnactiveException;
+
+public final class UserActivityPolicy {
+
+    public static void validateActivity(UserActivityView userActivityView) {
+        if (userActivityView.getStatus() == UserStatus.BLOCKED) {
+            throw new UserBlockedException("User is blocked.");
+        }
+
+        if (!userActivityView.isEmailVerified()) {
+            throw new UserEmailNotVerifiedException("User email is not verified.");
+        }
+
+        if (userActivityView.getStatus() != UserStatus.ACTIVE) {
+            throw new UserIsUnactiveException("User is unactive.");
+        }
+    }
+}
